aafc3eb76ef4ce81050ce6781bad9ed8d15671e3
Ethan Zammit <ethanzammit@gmail.com>
2020-11-15 19:06:12 +0100

    might have found the problem (assign function not wokring as intended)

--- Task1/Main.java
+++ Task1/Main.java
@@ -7,6 +7,7 @@ public class Main {
     public static void main(String args[]) {
         String exampleImg = "TTTTTTTT\n" + "TTTTTTTT\n" + "TTTTFFFT\n" + "TTTTTTFF\n" + "FFFFTTFF\n" + "FFFFTTFF\n"
                 + "FFFFTTTT\n" + "FFFFTTTF\n";
+                
         // System.out.println("Starting!");
         char[] imgArray = exampleImg.replaceAll("\n", "").toCharArray();
         int sideLength = (int) Math.sqrt(imgArray.length);
@@ -15,15 +16,34 @@ public class Main {
         tree.init(1, sideLength * sideLength);
         tree.assign(imgArray);
 
+        System.out.println("Diff checker");
+        System.out.println("assigned");
         for (int i = 1; i <= imgArray.length; i++) {
             System.out.print(tree.getPixelByChar(i));
         }
+        
         System.out.println();
+        System.out.println("original");
         for (int i = 1; i <= imgArray.length; i++) {
             System.out.print(imgArray[i - 1]);
         }
 
         System.out.println("\n\n");
+        System.out.println("test indexes");
+        
+        int j =0;
+        for (int i : tree.indexShifter()) {
+            System.out.print(i+"\t");
+            j++;
+
+            if(j==8){
+                System.out.println();
+                j = 0;
+            }
+        }
+
+        System.out.println("\n\n");
+        System.out.println("Assigned tree print");
         System.out.println(tree.print());
         System.out.println("\n\n");
         System.out.println("Pre optimization leaf count = " + tree.leafCount());
@@ -34,5 +54,6 @@ public class Main {
         System.out.println("\n\n");
         System.out.println(tree.print());
         System.out.println("\n\n");
+
     }
 }
