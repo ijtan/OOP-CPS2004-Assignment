12d718308b21aece6aff2f792da7dbee4fb87906
Ethan Zammit <ethanzammit@gmail.com>
2020-12-20 12:56:32 +0100

    testing templates and overloading operators

--- Task3/main.cpp
+++ Task3/main.cpp
@@ -1,99 +1,27 @@
 #include <iostream>
 #include <vector>
+#include "myuint.h"
 #define ll long long
 // long long holds 8 bytes
 using namespace std;
 
-template <ll size>
-class myuint
-{
-private:
-    vector<ll> values = vector<ll>((size / 8) / sizeof(ll)); // elements
-public:
-    template <ll otherSize>
-    myuint<size + otherSize> operator+(myuint<otherSize> &other)
-    {
-        cout << "+ operator called!" << endl;
-        return add<size + otherSize>(other);
-    };
 
-
-
-    template <ll otherSize>
-    myuint<(size > otherSize ? size : otherSize)> operator-(myuint<otherSize> &other)
-    {
-        cout << "- operator called!" << endl;
-        return sub<(size > otherSize ? size : otherSize)>(other);
-    };
-
-    template <ll otherSize>
-    myuint operator=(const myuint &p) const;
-
-    // myuint operator&(const myuint &p) const;
-    // myuint operator|(const myuint &p) const;
-    // myuint operator^(const myuint &p) const;
-    // myuint operator<<(const myuint &p) const;
-    // myuint operator>>(const myuint &p) const;
-
-    // bool operator~(const myuint &p) const;
-    // bool operator/(const myuint &p) const;
-    // bool operator%(const myuint &p) const;
-    // bool operator*(const myuint &p) const;
-
-    // bool operator<(const myuint &p) const;
-    // bool operator>(const myuint &p) const;
-
-    vector<ll> getValues()
-    {
-        return values;
-    }
-    int getSize()
-    {
-        return size;
-    }
-
-    myuint(ll value)
-    {
-        cout << "init with size: " << size << " and value: " << value << endl;
-        // this.assign(value);
-    }
-
-    myuint()
-    {
-        cout << "init with size: " << size << " and no value" << endl;
-        int currentSize = size;
-    }
-
-    template <ll addResultSize, ll otherSize>
-    myuint<addResultSize> add(myuint<otherSize> &b)
-    {
-        cout << "in add method" << endl;
-        ll aValue = getValues().back();
-        ll bValue = b.getValues().back();
-        return aValue + bValue;
-    }
-
-    template <ll subResultSize, ll otherSize>
-    myuint<subResultSize> sub(myuint<otherSize> &b)
-    {
-        cout << "in sub method" << endl;
-        ll aValue = getValues().back();
-        ll bValue = b.getValues().back();
-        return aValue - bValue;
-    }
-};
 
 
 int main()
 {
-    // myuint(4)::myuint operator+(const myuint &other) const { return add(this, other); };
     cout << "size of long in bytes is: " << sizeof(ll) << endl;
-    cout << "size of long in bits is: " << sizeof(ll) * 8 << endl;
-    myuint<64> i;
+    cout << "size of long in bits is: " << sizeof(ll) * 8 /*(<-- size of bit)*/ << endl;
+    myuint<32> i;
+    cout << "i size is: " << i.getSize() << endl;
+
     myuint<2048> j;
+    cout << "j size is: " << j.getSize() << endl;
+
     myuint x = i + j;
-    myuint y = i - j;
     cout << "x size is: " << x.getSize() << endl;
+
+    myuint y = i - j;    
     cout << "y size is: " << y.getSize() << endl;
 
     return 0;
---
+++ Task3/myuint.h
@@ -0,0 +1,88 @@
+#include <iostream>
+#include <vector>
+#define ll long long
+using namespace std;
+
+template <ll size>
+class myuint
+{
+private:
+    vector<ll> values = vector<ll>((size / sizeof(ll) >=1)? size / sizeof(ll) : 1); // check if size < 1 //TODO as we should hold minimum of 1 bit!;
+public:
+    template <ll otherSize>
+    myuint<size + otherSize> operator+(myuint<otherSize> &other)
+    {
+        cout << "+ operator called!\n";
+        return add<size + otherSize>(other);
+    };
+
+
+
+    template <ll otherSize>
+    myuint<(size > otherSize ? size : otherSize)> operator-(myuint<otherSize> &other)
+    {
+        cout << "- operator called!\n";
+        return sub<(size > otherSize ? size : otherSize)>(other);
+    };
+
+    template <ll otherSize>
+    myuint operator=(const myuint &p) const;
+
+    // myuint operator&(const myuint &p) const;
+    // myuint operator|(const myuint &p) const;
+    // myuint operator^(const myuint &p) const;
+    // myuint operator<<(const myuint &p) const;
+    // myuint operator>>(const myuint &p) const;
+
+    // bool operator~(const myuint &p) const;
+    // bool operator/(const myuint &p) const;
+    // bool operator%(const myuint &p) const;
+    // bool operator*(const myuint &p) const;
+
+    // bool operator<(const myuint &p) const;
+    // bool operator>(const myuint &p) const;
+
+    vector<ll> getValues()
+    {
+        return values;
+    }
+
+    int getSize()
+    {
+        // return sizeof(values)/sizeof(values[0]);
+        return values.size()*sizeof(values[0]);
+    }
+
+    myuint(ll value)
+    {
+        cout << "init with size: " << size << " and value: " << value <<'\n';
+        // this.assign(value);
+    }
+
+    myuint()
+    {
+        cout << "init with size: " << size << " and no value\n";
+        int currentSize = size;
+    }
+
+    template <ll addResultSize, ll otherSize>
+    myuint<addResultSize> add(myuint<otherSize> &b)
+    {
+        cout << "in add method\n";
+        ll aValue = getValues().back();
+        ll bValue = b.getValues().back();
+        if(sizeof(aValue + bValue) > addResultSize){
+            cout<<"size of value > size of return!\n";
+        }
+        return aValue + bValue;
+    }
+
+    template <ll subResultSize, ll otherSize>
+    myuint<subResultSize> sub(myuint<otherSize> &b)
+    {
+        cout << "in sub method\n";
+        ll aValue = getValues().back();
+        ll bValue = b.getValues().back();
+        return aValue - bValue;
+    }
+};
