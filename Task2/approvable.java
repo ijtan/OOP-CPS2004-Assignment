dbfef4497511d03404ff4e40ce0f06aaa77da8d3
Ethan Zammit <ethanzammit@gmail.com>
2021-01-13 20:31:15 +0100

    trying to check what serialVersionUID for serializables

--- Task2/approvable.java
+++ Task2/approvable.java
@@ -2,20 +2,11 @@ package Task2;
 
 import java.io.Serializable;
 
-// public abstract interface requestHandler {
-//     public void approve(request r);
+public abstract class approvable implements Serializable{
 
-//     public void decline(request r);
-// }
+    // private static final long serialVersionUID = -8490485024370728923L;
 
-public abstract class approvable{
-        // public static void approve(request r) throws Exception {
-        //     throw new Exception("This should be overriden to handle object approval");
-        // };
-        // public static void decline(request r) throws Exception {
-        //     throw new Exception("This should be overriden to handle object decline");
-        // };
-        public abstract void approve(request r);
+    public abstract void approve(request r);
         public abstract void decline(request r);
     
 }
