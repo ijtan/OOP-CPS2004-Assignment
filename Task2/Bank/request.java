a860e194e93e360ee866e85b9acf97aa7b758d8d
Ethan Zammit <ethanzammit@gmail.com>
2021-01-21 17:52:00 +0100

    Task2: removed unused comments

--- Task2/Bank/request.java
+++ Task2/Bank/request.java
@@ -6,24 +6,12 @@ import java.util.Map;
 
 public class request implements Serializable {
     private String requesterID;
-    // private Map<String, Ob/ject> params = new HashMap<String, Object>();
     approverInterface approver;
-
-    // public request(String requesterID, Map<String, Object> params) {
-    //     this.requesterID = requesterID;
-    //     this.params = params;
-    // }
 
     public request(String requesterID, approverInterface appInt) {
         this.requesterID = requesterID;
         this.approver = appInt;
     }
-
-    // public request(String requesterID, approverInterface appInt, Map<String, Object> params) {
-    //     this.requesterID = requesterID;
-    //     this.approver = appInt;
-    //     this.params = params;
-    // }
 
     public void approve(){
         approver.approve(this);
@@ -32,16 +20,4 @@ public class request implements Serializable {
     public String getRequester() {
         return requesterID;
     }
-
-    // public Object getParam(String key) throws Exception {
-    //     Object obj = params.get(key);
-    //     if (obj == null)
-    //         throw new Exception("Key not associated with any value!");
-    //     return obj;
-    // }
-
-    // public Object getParamNoExcept(String key) {
-    //     Object obj = params.get(key);
-    //     return obj;
-    // }
 }
